a58d52f0f73aaadc3c66f75eb352290a7835afbb
Thierry Reding <treding@nvidia.com>
2013-08-13 14:04:49 +0200

    ARM: PCI: Export pci_common_init_dev() and pci_common_exit()
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- arch/arm/kernel/bios32.c
+++ arch/arm/kernel/bios32.c
@@ -566,6 +566,7 @@ void pci_common_init_dev(struct device *parent, struct hw_pci *hw)
 		}
 	}
 }
+EXPORT_SYMBOL(pci_common_init_dev);
 
 void pci_common_exit(struct list_head *head)
 {
@@ -584,6 +585,7 @@ void pci_common_exit(struct list_head *head)
 		kfree(sys);
 	}
 }
+EXPORT_SYMBOL(pci_common_exit);
 
 #ifndef CONFIG_PCI_HOST_ITE8152
 void pcibios_set_master(struct pci_dev *dev)
