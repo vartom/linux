7aebac0500982878e7d25a3d4d81c69353cb4e38
Thierry Reding <treding@nvidia.com>
2014-05-13 11:36:13 +0200

    drm: Use size_t for blob property sizes
    size_t is the standard type when dealing with sizes of all kinds. Use it
    consistently when instantiating DRM blob properties.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/gpu/drm/drm_crtc.c
+++ drivers/gpu/drm/drm_crtc.c
@@ -3938,8 +3938,9 @@ done:
 	return ret;
 }
 
-static struct drm_property_blob *drm_property_create_blob(struct drm_device *dev, int length,
-							  void *data)
+static struct drm_property_blob *
+drm_property_create_blob(struct drm_device *dev, size_t length,
+			 void *data)
 {
 	struct drm_property_blob *blob;
 	int ret;
@@ -4053,7 +4054,8 @@ int drm_mode_connector_update_edid_property(struct drm_connector *connector,
 					    struct edid *edid)
 {
 	struct drm_device *dev = connector->dev;
-	int ret, size;
+	size_t size;
+	int ret;
 
 	/* ignore requests to set edid when overridden */
 	if (connector->override_edid)
