90ed130a1772f5d37088a2cda0551de2fc125c53
Thierry Reding <treding@nvidia.com>
2014-10-21 14:15:35 +0200

    mm/kmemleak: Do not skip stack frames
    Trying to chase down memory leaks is much easier when the complete stack
    trace is available.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>
    ---
    It seems like this was initially set to 1 when merged in commit
    3c7b4e6b8be4 (kmemleak: Add the base support) and later increased to 2
    in commit fd6789675ebf (kmemleak: Save the stack trace for early
    allocations). Perhaps there was a reason to skip the first few frames,
    but I've certainly found it difficult to find leaks when the stack trace
    doesn't point at the proper location.

--- mm/kmemleak.c
+++ mm/kmemleak.c
@@ -526,7 +526,7 @@ static int __save_stack_trace(unsigned long *trace)
 	stack_trace.max_entries = MAX_TRACE;
 	stack_trace.nr_entries = 0;
 	stack_trace.entries = trace;
-	stack_trace.skip = 2;
+	stack_trace.skip = 0;
 	save_stack_trace(&stack_trace);
 
 	return stack_trace.nr_entries;
