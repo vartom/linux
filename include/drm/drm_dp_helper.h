b74b402ef276a66238615955a2eb85aa2fabc630
Thierry Reding <treding@nvidia.com>
2016-05-26 09:33:06 +0200

    WIP: DP link rate

--- include/drm/drm_dp_helper.h
+++ include/drm/drm_dp_helper.h
@@ -875,7 +875,15 @@ struct drm_dp_link {
 
 	unsigned int rate;
 	unsigned int lanes;
+
+	/* eDP 1.4 SUPPORTED_LINK_RATES in kHz */
+	unsigned long rates[DP_MAX_SUPPORTED_RATES];
+	unsigned int num_rates;
 };
+
+int drm_dp_link_add_rate(struct drm_dp_link *link, unsigned long rate);
+int drm_dp_link_remove_rate(struct drm_dp_link *link, unsigned long rate);
+void drm_dp_link_update_rates(struct drm_dp_link *link);
 
 int drm_dp_link_probe(struct drm_dp_aux *aux, struct drm_dp_link *link);
 int drm_dp_link_power_up(struct drm_dp_aux *aux, struct drm_dp_link *link);
