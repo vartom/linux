8dfa73460b85dbc47bb8e88845c7b32d471e2523
Thierry Reding <treding@nvidia.com>
2017-04-03 11:40:03 +0200

    gpio: Move irqdomain into struct gpio_irq_chip
    In order to consolidate the multiple ways to associate an IRQ chip with
    a GPIO chip, move more fields into the new struct gpio_irq_chip.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/pinctrl/pinctrl-amd.c
+++ drivers/pinctrl/pinctrl-amd.c
@@ -531,7 +531,7 @@ static void amd_gpio_irq_handler(struct irq_desc *desc)
 						(off * 4 + i) * 4);
 				if ((pin_reg & BIT(INTERRUPT_STS_OFF)) ||
 					(pin_reg & BIT(WAKE_STS_OFF))) {
-					irq = irq_find_mapping(gc->irqdomain,
+					irq = irq_find_mapping(gc->irq.domain,
 								off * 4 + i);
 					generic_handle_irq(irq);
 					writel(pin_reg,
