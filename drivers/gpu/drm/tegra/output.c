51fdda92716abd90e2d494eff0ffb1980a258186
Hans Verkuil <hans.verkuil@cisco.com>
2017-09-11 14:29:52 +0200

    drm/tegra: hdmi: Add cec-notifier support
    In order to support CEC the HDMI driver has to inform the CEC driver
    whenever the physical address changes. So when the EDID is read the
    CEC driver has to be informed and whenever the hotplug detect goes
    away.
    
    This is done through the cec-notifier framework.
    
    The link between the HDMI driver and the CEC driver is done through
    the hdmi-phandle property in the tegra-cec node in the device tree.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/gpu/drm/tegra/output.c
+++ drivers/gpu/drm/tegra/output.c
@@ -38,6 +38,7 @@ int tegra_output_connector_get_modes(struct drm_connector *connector)
 
 	cec_notifier_set_phys_addr_from_edid(output->notifier, edid);
 	drm_mode_connector_update_edid_property(connector, edid);
+	cec_notifier_set_phys_addr_from_edid(output->notifier, edid);
 
 	if (edid) {
 		err = drm_add_edid_modes(connector, edid);
