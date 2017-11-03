c3d6b9c4e057e46ce9487bd3aa701583bebee1e8
Thierry Reding <treding@nvidia.com>
2017-11-03 15:23:16 +0100

    Revert "mm: drop migrate type checks from has_unmovable_pages"
    This reverts commit e1d753dff0faadbdc6be5a223959e4d2bcddb059.

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -7470,6 +7470,9 @@ bool has_unmovable_pages(struct zone *zone, struct page *page, int count,
 	 */
 	if (zone_idx(zone) == ZONE_MOVABLE)
 		return false;
+	mt = get_pageblock_migratetype(page);
+	if (mt == MIGRATE_MOVABLE || is_migrate_cma(mt))
+		return false;
 
 	/*
 	 * CMA allocations (alloc_contig_range) really need to mark isolate
