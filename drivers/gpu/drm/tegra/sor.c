7a9e64022e71ad3113828d4a33bb1cf0a335bab7
Thierry Reding <treding@nvidia.com>
2017-11-09 12:37:11 +0100

    drm/tegra: sor: Add code to enable register tracing
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/gpu/drm/tegra/sor.c
+++ drivers/gpu/drm/tegra/sor.c
@@ -29,6 +29,7 @@
 #include "sor.h"
 #include "trace.h"
 
+#define SOR_TRACE_REGISTERS 0
 #define SOR_REKEY 0x38
 
 struct tegra_sor_hdmi_settings {
@@ -390,6 +391,9 @@ static inline u32 tegra_sor_readl(struct tegra_sor *sor, unsigned int offset)
 {
 	u32 value = readl(sor->regs + (offset << 2));
 
+	if (SOR_TRACE_REGISTERS)
+		dev_dbg(sor->dev, "%08x > %08x\n", offset, value);
+
 	trace_sor_readl(sor->dev, offset, value);
 
 	return value;
@@ -399,6 +403,10 @@ static inline void tegra_sor_writel(struct tegra_sor *sor, u32 value,
 				    unsigned int offset)
 {
 	trace_sor_writel(sor->dev, offset, value);
+
+	if (SOR_TRACE_REGISTERS)
+		dev_dbg(sor->dev, "%08x < %08x\n", offset, value);
+
 	writel(value, sor->regs + (offset << 2));
 }
 
